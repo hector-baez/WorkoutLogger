edf4a7ea229cc77e674377e7e77e255f5047ad13
hector-baez <hbaez@oxy.edu>
2022-11-27 22:34:52 -0600

    removed tracker modules

--- tracker/tracker_data/src/main/java/com/hbaez/tracker_data/remote/dto/SearchDto.kt
+++
@@ -1,5 +0,0 @@
-package com.hbaez.tracker_data.remote.dto
-
-data class SearchDto(
-    val products: List<Product>,
-)
--- tracker/tracker_domain/src/main/java/com/hbaez/tracker_domain/use_case/TrackerUseCases.kt
+++
@@ -1,9 +0,0 @@
-package com.hbaez.tracker_domain.use_case
-
-data class TrackerUseCases(
-    val trackFood: TrackFood,
-    val searchFood: SearchFood,
-    val getFoodsForDate: GetFoodsForDate,
-    val deleteTrackedFood: DeleteTrackedFood,
-    val calculateMealNutrients: CalculateMealNutrients
-)
